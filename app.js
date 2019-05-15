5b9e53a8ef5531eb5ce4f55352b454c29e8d7849
Taylor Hill <ineedfree360s@aol.com>
2019-05-15 08:41:30 -0400

    Updating all files with the entire script

---
+++ app.js
@@ -0,0 +1,15 @@
+var display = document.getElementById('display')
+
+function appendNumber(number) {
+  var stringValue = String(number)
+  display.innerHTML += stringValue
+}
+
+function solve() {
+  var items = display.innerHTML.split('+')
+  display.innerHTML = Number(items[0]) + Number(items[1])
+}
+
+function cDisplay() {
+  display.innerHTML = ' '
+}
